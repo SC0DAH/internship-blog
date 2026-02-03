a33e011c566dd920a42b1c0af5ae610506fcf554
SDPSN\SenneCockx <senne.cockx@esc.be>
2026-02-03 15:49:19 +0100

    added nav-bar

--- app/app.vue
+++ app/app.vue
@@ -3,37 +3,37 @@ import { HomeIcon, Bars3Icon, UserIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
-  <nav class="fixed w-full bg-white/70 backdrop-blur-sm border-b border-gray-100 z-50 font-sans">
-    <div class="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
+  <nav class="bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default">
+    <div class="max-w-screen-xl flex items-center justify-between mx-auto p-4">
       
-      <!-- Logo -->
-      <div class="text-xl font-semibold cursor-pointer">
-        sennecx
-      </div>
+      <RouterLink to="/" class="inline-flex items-center justify-center h-10 px-4 rounded-full text-heading hover:bg-black/10 hover:text-black transition-colors">
+        <span class="self-center text-xl text-heading font-semibold whitespace-nowrap">sennecx</span>
+      </RouterLink>
 
-      <!-- Desktop Menu -->
-      <div class="hidden md:flex items-center gap-6">
-        <button class="w-8 h-8 text-gray-600 hover:text-black transition-colors">
-          <HomeIcon class="w-6 h-6"/>
+      <ul class="hidden md:flex flex-1 justify-center items-center space-x-8 rtl:space-x-reverse font-medium">
+        <li>
+          <RouterLink to="/" class="inline-flex items-center justify-center h-10 px-4 rounded-full text-heading hover:bg-black/10 hover:text-black transition-colors">Home</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/blogs" class="inline-flex items-center justify-center h-10 px-4 rounded-full text-heading hover:bg-black/10 hover:text-black transition-colors">Blogs</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/internship" class="inline-flex items-center justify-center h-10 px-4 rounded-full text-heading hover:bg-black/10 hover:text-black transition-colors">Internship</RouterLink>
+        </li>
+      </ul>
+
+      <RouterLink to="/profile" class="hidden md:flex items-center justify-center w-10 h-10 rounded-full text-heading hover:text-black hover:bg-black/10 transition-colors">
+        <UserIcon class="w-6 h-6"/>
+      </RouterLink>
+
+      <div class="flex md:hidden">
+        <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base hover:bg-neutral-secondary-soft focus:outline-none focus:ring-2 focus:ring-neutral-tertiary" aria-controls="navbar-sticky" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
+          </svg>
         </button>
-
-        <!-- Nav Links -->
-        <button class="text-gray-600 hover:text-black transition-colors">Blog</button>
-        <button class="text-gray-600 hover:text-black transition-colors">Skills</button>
-        <button class="text-gray-600 hover:text-black transition-colors">Internship</button>
-        <button class="w-8 h-8 text-gray-600 hover:text-black transition-colors">
-          <UserIcon class="w-6 h-6"/>
-        </button>
       </div>
-
-      <!-- Mobile Menu -->
-      <div class="flex items-center md:hidden gap-6">
-        <button class="text-gray-600">
-          <Bars3Icon class="w-6 h-6"/>
-        </button>
-      </div>
-
     </div>
   </nav>
 </template>
-
