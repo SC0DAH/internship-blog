b286d81c5c83cb321a5462cffe35eff520d3f2a0
SDPSN\SenneCockx <senne.cockx@esc.be>
2026-02-03 16:50:21 +0100

    made hamburger menu

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { HomeIcon, Bars3Icon, UserIcon } from '@heroicons/vue/24/outline'
+import {ref } from 'vue'
+
+const isOpen = ref(false);
 </script>
 
 <template>
@@ -27,13 +30,36 @@ import { HomeIcon, Bars3Icon, UserIcon } from '@heroicons/vue/24/outline'
       </RouterLink>
 
       <div class="flex md:hidden">
-        <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base hover:bg-neutral-secondary-soft focus:outline-none focus:ring-2 focus:ring-neutral-tertiary" aria-controls="navbar-sticky" aria-expanded="false">
-          <span class="sr-only">Open main menu</span>
-          <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
-          </svg>
+        <button @click="isOpen = !isOpen" data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base hover:bg-neutral-secondary-soft focus:outline-none focus:ring-2 focus:ring-neutral-tertiary w-10 h-10 rounded-full text-heading hover:text-black hover:bg-black/10 transition-colors" aria-controls="navbar-sticky" aria-expanded="false">
+          <Bars3Icon class="w-6 h-6"/>
         </button>
+        <RouterLink to="/profile" class="flex items-center justify-center w-10 h-10 rounded-full text-heading hover:text-black hover:bg-black/10 transition-colors">
+        <UserIcon class="w-6 h-6"/>
+      </RouterLink>
       </div>
     </div>
   </nav>
+
+  <div
+    v-if="isOpen"
+    class="md:hidden fixed top-[72px] left-0 w-full z-10 flex justify-center"
+  >
+    <ul class="flex flex-col item-center gap-2 p-4 font-medium">
+      <li>
+        <RouterLink @click="isOpen = false" to="/" class="w-full max-w-xs inline-flex items-center justify-center h-11 px-4 rounded-full text-heading hover:text-black hover:bg-black/10 transition-colors">
+          Home
+        </RouterLink>
+      </li>
+      <li>
+        <RouterLink @click="isOpen = false" to="/blogs" class="w-full max-w-xs inline-flex items-center justify-center h-11 px-4 rounded-full text-heading hover:text-black hover:bg-black/10 transition-colors">
+          Blogs
+        </RouterLink>
+      </li>
+      <li>
+        <RouterLink @click="isOpen = false" to="/internship" class="w-full max-w-xs inline-flex items-center justify-center h-11 px-4 rounded-full text-heading hover:text-black hover:bg-black/10 transition-colors">
+          Internship
+        </RouterLink>
+      </li>
+    </ul>
+  </div>
 </template>
