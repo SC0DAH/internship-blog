2b4011938aebddc09310a9768bee41ca5889772c
SDPSN\SenneCockx <senne.cockx@esc.be>
2026-02-06 16:55:33 +0100

    changed intro section

--- app/components/IntroSection.vue
+++ app/components/IntroSection.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="text-center py-24">
     <h1 class="text-4xl md:text-5xl font-bold text-heading">
-      Ik ben Senne Cockx
+      Senne Cockx
     </h1>
     <p class="text-lg md:text-xl text-neutral-700 mt-6">
       Junior Developer met een passie voor het creeren van mooie, functionele web applicaties.
