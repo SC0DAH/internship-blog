b2d18c1c5ae7fa4cef6997e801e60a42be4c6f38
SDPSN\SenneCockx <senne.cockx@esc.be>
2026-02-06 11:14:38 +0100

    added middleware that checks if the user is logged in

---
+++ app/middleware/my-middleware.ts
@@ -0,0 +1,14 @@
+import { defineNuxtRouteMiddleware, navigateTo } from '#app'
+import { useAuth } from '~~/composables/useAuth'
+
+export default defineNuxtRouteMiddleware(async (to) => {
+  const { user, loading } = useAuth()
+
+  while (loading.value) {
+    await new Promise(resolve => setTimeout(resolve, 50))
+  }
+
+  if (!user.value) {
+    return navigateTo('/login')
+  }
+})
