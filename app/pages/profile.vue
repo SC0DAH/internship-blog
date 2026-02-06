c091dcd29bc820b51ae072b346077ee300fd5207
SDPSN\SenneCockx <senne.cockx@esc.be>
2026-02-06 11:16:57 +0100

    added a profile page showing the users information

---
+++ app/pages/profile.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useAuth } from '~~/composables/useAuth'
+definePageMeta({
+  middleware: 'my-middleware'
+})
+const { user } = useAuth()
+</script>
+
+<template>
+    <div>
+        <p>Profile page</p>
+        <p>Email: {{ user?.email }}</p>
+        <p>Display Name: {{ user?.displayName || 'No display name' }}</p>
+        <p>Creation Time: {{ user?.metadata.creationTime }}</p>
+        <p>Last Sign In Time: {{ user?.metadata.lastSignInTime }}</p>
+    </div>
+</template>
